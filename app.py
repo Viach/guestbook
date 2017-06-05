62ce41ec55f9a1825767e08db90115ee241b1711
Viach <vch.380@gmail.com>
2017-06-05 14:33:13 +0300

    alpha

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, json
-from flask_restful import Resource, Api, reqparse
-from guestbook.models import db, Guest
+from flask import Flask, render_template
+from flask_restful import  Api
+from guestbook.models import db, GuestJSONAPI, GuestXMLAPI
 
 app = Flask(__name__)
 app.config.from_pyfile('setup.py')
@@ -9,59 +9,13 @@ api = Api(app)
 db.init_app(app)
 
 
-class GuestAPI(Resource):
-    def __init__(self):
-        self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('id', type=int, location='json')
-        self.reqparse.add_argument('name', type=str, location='json')
-        self.reqparse.add_argument('email', type=str, location='json')
-        super(GuestAPI, self).__init__()
-
-    def get(self, pk=None):
-        if pk:
-            guest = Guest.query.get(ident=pk)
-            return guest.to_json() if guest else {'Error': 'Something wrong in request data'}
-        else:
-            guests = Guest.query.all()
-            r = {'guests': [guest.to_json() for guest in guests]}
-            return r
-
-    def post(self):
-        args = self.reqparse.parse_args()
-        if args['name'] and not args['id']:
-            guests = Guest.query.order_by(Guest.id).all()
-            pk = guests[-1].id + 1
-            guest = Guest(name=args['name'], email=args['email'], pk=pk)
-            db.session.add(guest)
-            db.session.commit()
-            return {'Status': 'Added'}
-        return {'Error': 'Error in POST'}
-
-    def delete(self, pk=None):
-        try:
-            guest = Guest.query.get(ident=pk)
-        except:
-            return {'Error': 'Something wrong in request data'}
-        db.session.delete(guest)
-        db.session.commit()
-        return {'Status': 'Deleted'}
-
-    def put(self, pk=None):
-        try:
-            guest = Guest.query.get(ident=pk)
-        except:
-            return {'Error': 'Something wrong in request data'}
-        guest.email = '&&&&&&&&&&&&&&&&'
-        db.session.commit()
-        return {'Status': 'Deleted'}
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-api.add_resource(GuestAPI, '/api/guests/<pk>', '/api/guests/')
+api.add_resource(GuestJSONAPI, '/api/guests/<pk>.json', '/api/guests/.json')
+api.add_resource(GuestXMLAPI, '/api/guests/<pk>.xml', '/api/guests/.xml')
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,4 +1,8 @@
+import xml.etree.ElementTree as ET
+
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, reqparse
+from flask import Response, request, json
 
 db = SQLAlchemy()
 
@@ -13,7 +17,6 @@ class Guest(db.Model):
         self.name = name
         self.email = email
 
-
     def __repr__(self):
         return '<Guest {}>'.format(self.name)
 
@@ -21,4 +24,83 @@ class Guest(db.Model):
         return {'id': self.id, 'name': self.name, 'email': self.email}
 
     def to_xml(self):
-        return '<id>{}</id>\n<name>{}</name>\n<email>{}</email>'.format(self.id, self.name, self.email)
+        return '<guest>\n<id>{}</id>\n<name>{}</name>\n<email>{}</email>\n</guest>'.format(self.id, self.name,
+                                                                                           self.email)
+
+
+class GuestAPI(Resource):
+    def __init__(self):
+        self.location = request.url.rpartition('.')[-1]
+        self.resp = Response(status=200, mimetype='application/' + self.location)
+        super(GuestAPI, self).__init__()
+
+    def __set_data(self):
+        if self.location == 'json':
+            data = json.loads(request.data)
+            self.id = data.get('id', None)
+            self.name = data.get('name', None)
+            self.email = data.get('email', None)
+        elif self.location == 'xml':
+            data = request.data
+            data = ET.fromstring(data.decode())
+            self.id = data.findtext('id')
+            self.name = data.findtext('name')
+            self.email = data.findtext('email')
+        else:
+            return 'Error in setting data'
+
+    def get(self, pk=None):
+        if pk:
+            guest = Guest.query.get(ident=pk)
+            data = {'json': json.dumps(guest.to_json()), 'xml': guest.to_xml()}[self.location]
+        else:
+            guests = Guest.query.all()
+            data = {'json': json.dumps({'guests': [guest.to_json() for guest in guests]}),
+                    'xml': '<guests>{}</guests>'.format('\n'.join([guest.to_xml() for guest in guests]))} \
+                [self.location]
+        self.resp.data = data
+        return self.resp
+
+    def delete(self, pk=None):
+        try:
+            guest = Guest.query.get(ident=pk)
+            db.session.delete(guest)
+            db.session.commit()
+        except Exception as e:
+            return str(e)
+        return
+
+    def post(self):
+        self.__set_data()
+        if self.name and not self.id:
+            try:
+                guests = Guest.query.order_by(Guest.id).all()
+                pk = guests[-1].id + 1 if guests else 1
+                guest = Guest(name=self.name, email=self.email, pk=pk)
+                db.session.add(guest)
+                db.session.commit()
+                return 'Added'
+            except Exception as e:
+                return 'Error ', str(e)
+        return 'Error ', self
+
+    def put(self, pk=None):
+        self.__set_data()
+        if str(self.id) != str(pk):
+            return 'Error in id'
+        try:
+            guest = Guest.query.get(ident=pk)
+            guest.email = self.email
+            guest.name = self.name
+            db.session.commit()
+        except Exception as e:
+            return 'Error', str(e)
+        return 'Updated'
+
+
+class GuestJSONAPI(GuestAPI):
+    pass
+
+
+class GuestXMLAPI(GuestAPI):
+    pass
