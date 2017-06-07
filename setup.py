99ae7f947c9fda3da577ca43cc1fb32746200e57
Viach <vch.380@gmail.com>
2017-06-08 00:24:52 +0300

    done

--- setup.py
+++ setup.py
@@ -1,2 +1,13 @@
-SQLALCHEMY_DATABASE_URI = 'sqlite:///guestbook.db'
+
+# PostgreSQL manages concurrency through a system known as
+# multiversion concurrency control (MVCC),
+# which gives each transaction a "snapshot" of the database,
+# allowing changes to be made without being visible to other transactions
+# until the changes are committed.
+
+# For best performance you should use PostgreSQL on production server:
+# postgresql://apana:trana@localhost/mydatabase
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///guestbook.db'   # Use SQLite for developing mode only
+
 DEBUG = False
