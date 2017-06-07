6c96c2be3961a263e10a733d1fc3cf57244166e8
Viach <vch.380@gmail.com>
2017-06-07 22:06:26 +0300

    work with vue put done

--- static/guestbook.js
+++ static/guestbook.js
@@ -7,13 +7,38 @@ Vue.component('modal', {
     }
 });
 
+Vue.component('modal-edit', {
+    template: '#modal-edit-template',
+    props: ['message'],
+    methods: {
+        editGuest: function (guest) {
+            axios.put('/api/guests/' + guest.id + '.json',
+                {
+                    id: guest.id,
+                    name: guest.name,
+                    email: guest.email
+                }
+            ).then(function (response) {
+                    this.guests = response.data.guests;
+                }.bind(this)
+            ).catch(function (error) {
+                console.log(error);
+            });
+
+        }
+    },
+    created: function () {
+        // alert();
+    }
+});
 
 new Vue({
     el: "#app",
     data: {
         guests: [],
-        guestModal : [],
-        showModal: false
+        guestModal: [],
+        showModal: false,
+        editModal: false
     },
     methods: {
         getList: function () {
@@ -24,25 +49,6 @@ new Vue({
                 .catch(function (error) {
                     console.log(error);
                 });
-        },
-        showGuest: function (guest) {
-            alert('Guest info:\n' + guest.id + '\n' + guest.name + '\n' + guest.email);
-        },
-        editGuest: function (guest) {
-            axios.put('/api/guests/' + guest.id + '.json',
-                {
-                    id: guest.id,
-                    name: 'NEW',
-                    email: 'ENAIL'
-                }
-            )
-                .then(function (response) {
-                    this.guests = response.data.guests;
-                }.bind(this))
-                .catch(function (error) {
-                    console.log(error);
-                });
-            this.getList();
         },
         delGuest: function (guest) {
             axios.delete('/api/guests/' + guest.id + '.json')
