19ee4a8d37e09327fa5a7363da4ad1fb76a37f6d
Viach <vch.380@gmail.com>
2017-06-07 08:31:35 +0300

    work with vue get

--- static/guestbook.js
+++ static/guestbook.js
@@ -1,9 +1,26 @@
-
-
-var app = new Vue({
-    el:"#app",
+new Vue({
+    el: "#app",
     data: {
-        message: 'Hello MY@@@ Vue!'
+        messa: [1,2,3]
+    },
+
+    methods: {
+        getList: function () {
+            var myGet = axios.get('/api/guests/.json')
+                .then(function (response) {
+                    console.log(response.status);
+                    return  response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+
+        }
+    },
+    created:function () {
+        this.getList();
     }
+
+
 });
 
